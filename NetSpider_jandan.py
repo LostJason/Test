e6d3ec7b98a190e3845d035032b55b4847eea88b
LostJason <juxiaosen@163.com>
2019-10-31 11:31:59 +0800

    学习网络爬虫，了解正则表达式、bs4、requests的用法：实现通过h2、a标签抓取jandan首页上的文章标题及链接地址，并保存入txt文本

--- NetSpider_jandan.py
+++ NetSpider_jandan.py
@@ -4,52 +4,44 @@
 # @Author   ：Jason Phillip
 # @File     ：.py
 
+from bs4 import BeautifulSoup
 import requests
 import re
-from bs4 import BeautifulSoup
+import os
 
 
-class JanDan():
+def save_file(str):
+    with open(r'e:\ttt\log.txt', 'wb') as file:
+        file.write(str)
 
-    def __init__(self,url):
-        self.headers =  {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/'
-                    '537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-        self.url = url
 
-    def get_html(self):
-        response = requests.get(self.url,self.headers)
-        self.html = response.content
-        return self.html
+def parser_addr(str):
+    """通过正则表达式解析http地址"""
+    parser = re.compile(r'https?:\/\/[a-zA-Z0-9|.|/|-]+\.html')
+    t = re.findall(parser, str)
+    return t
 
-    def get_text(self):
-        html = self.get_html()
-        soup = BeautifulSoup(html,"html.parser")
-        # 数组对象定义（用于存放对象）
-        content_list = []
-        # 按照CSS类名搜索tag的功能非常实用,但标识CSS类名的关键字 class 在Python中是保留字,使用 class 做参数会导致语法错误.从Beautiful Soup的4.1.1版本开始,可以通过 class_ 参数搜索有指定CSS类名的tag
-        # 查找dl标签class为dataItem02的所有dl标签
-        for tag in soup.find_all("div", class_="indexs"):
-            # 对tag的子节点进行循环输出
-            for child in tag.children:
-                text = self.parser_text(str(child))
-                content_list.append(text)
-        print(list(content_list))
 
-    def parser_text(self,str):
-        """找到并返回符合规则的字符串"""
-        str.split('<h2>')
-        parserrole = re.compile(r'^<h2>')
-        #parserrole = re.compile(r'[\u4e00-\u9fa5]+')
-        text = re.findall(parserrole,str)
-        self.content = text
-        return self.content
+def parser_content(str):
+    """通过正则表达式解析标签文本"""
+    parser = re.compile(
+        r'[\u4e00-\u9fa5|0-9|\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]+')
+    t = re.findall(parser, str)
+    return t
 
-    def savetofile(self,path):
-        html = self.get_html()
-        with open(path, 'wb') as file:
-            file.write(html)
 
 if __name__ == '__main__':
-    jd = JanDan('http://jandan.net/2019/10/29')
-    # jd.savetofile(r'e:\ttt\jd.txt')
-    jd.get_text()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/'
+                                  '537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+    responses = requests.get(r'http://jandan.net/2019/10/29',headers)
+    html = responses.content
+    soup = BeautifulSoup(html, "html.parser")
+
+    content_list = []
+
+    for tag in soup.find_all("h2"):
+        addr = tag.a.attrs['href']
+        txt = parser_content(tag.text)
+        content_list.append([txt[0], addr])
+
+    save_file(str(content_list).encode())
